636b5d203f5777e4f48b81e6c186a31388cc43c9
Daywison Silva <daywisonsilvasou@gmail.com>
2020-10-14 12:23:48 -0300

    componentes

---
+++ secao7/componentes/componente-intro/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <app-contador></app-contador>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+};
+</script>
+
+<style>
+</style>
---
+++ secao7/componentes/componente-intro/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Contador from './Contador.vue'
+
+Vue.config.productionTip = false
+Vue.component('app-contador', Contador)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
