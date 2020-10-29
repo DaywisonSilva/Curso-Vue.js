9136ec99f1b753380bab0398715f2ec3091872ad
Daywison Silva <daywisonsilvasou@gmail.com>
2020-10-29 11:03:16 -0300

    aprendendo sobre comunicação entre componentes

---
+++ secao8/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info />
+            <app-usuario-editar />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    data() {
+        return {
+            nome: 'Pedro'
+        }
+    },
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    methods: {
+        alterarNome() {
+            this.nome = 'Ana'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ secao8/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do usuário: <strong>{{ inverterNome() }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+/* Quando se altera no componente pai o valor passado para o componente filho e dá problemas de tipo, o recomendável é que no componente filho se diga qual é o tipo e demais informações da propriedade que o filho espera receber, assim o tipo de erro gerado no console vai ser mais fácil de ser corrigido, trazendo maior manutenibilidade ao código. */
+  props: {
+    //   nome: [String, Array]
+    nome: {
+        type: String,
+        // required: true,
+        // default: 'Anônimo'
+        default: function () {
+            return Array(10).fill(0).join(',')
+        }
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    inverterNome() {
+      return this.nome.split("").reverse().join("");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
